2349d71c20be38df1521ccbd24be00a7bb223afd
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-10 15:50:02 -0600

    Create author MVC components, edit model tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
+RSpec.describe "Author", type: :model do
   before :each do
     @author1 = Author.create!(name: 'Agatha Christie', dead: true, sales: 2000000000)
     @author2 = Author.create!(name: 'William Shakespeare', dead: true, sales: 2000000000)
   end
   
   describe "instance methods" do
-    describe '#index' do
-      it 'displays an index of authors by name' do
-        expect(@author1.index).to include(@author1.name)
-        expect(@author2.index).to include(@author2.name)
+    describe '#' do
+      it '' do
+
       end
     end
   end
