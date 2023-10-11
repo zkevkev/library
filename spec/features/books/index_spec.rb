88e256c058a0323d06ff19cdc8fc86f2c9366552
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-11 08:08:25 -0600

    Modify feature book tests to account for author id

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'books index page', type: :feature do
   before :each do
-    @book1 = Book.create!(name: 'Death on the Nile', new: false, publish_date: 1937)
-    @book2 = Book.create!(name: 'King Lear', new: true, publish_date: 1606)
+    @author1 = Author.create!(name: 'Agatha Christie', dead: true, sales: 2000000000)
+    @author2 = Author.create!(name: 'William Shakespeare', dead: true, sales: 2000000000)
+    @book1 = Book.create!(name: 'Death on the Nile', new: false, publish_date: 1937, author_id: @author1.id)
+    @book2 = Book.create!(name: 'King Lear', new: true, publish_date: 1606, author_id: @author2.id)
   end
 
   it 'shows an index of books, including their attributes' do
