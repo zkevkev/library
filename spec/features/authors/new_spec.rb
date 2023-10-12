b53dd20f2364d02f31ac8c6e8f7243c08cef976a
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-12 06:27:58 -0600

    Write additional testing for authors new feature

--- spec/features/authors/new_spec.rb
+++ spec/features/authors/new_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'authors new page', type: :feature do
+  it 'posts a new author to the authors table when submitted' do
+    visit "/parents/new"
+    # do more research on within (should maybe be used here)
+    fill_in 'name', with: 'Haruki Murakami'
+    fill_in 'copies sold', with: '100000000'
+    page.check('alive')
+    click_button 'Create Parent'
+    visit "/parents"
+
+    expect(page).to have_content('Haruki Murakami') 
+  end
+  
   it 'redirects to authors index when form is completed' do
     visit "/parents/new"
     # do more research on within (should maybe be used here)
