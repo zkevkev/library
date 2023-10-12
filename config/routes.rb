41aa32ce86e8453a563bfb48160524e9ed6184e9
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-12 04:38:51 -0600

    Add nav tag to top links

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/", to: "welcome#index"
+
   get "/parents", to: "authors#index"
   get "/parents/:id", to: "authors#show"
+
   get "/child_table_name", to: "books#index"
   get "/child_table_name/:id", to: "books#show"
+  
   get "/parents/:parent_id/child_table_name", to: "author_books#index"
 end
